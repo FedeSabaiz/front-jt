1d06ca08444fd9f083700f54ee1172e028388927
Federico <federico.gomez.sa@gmail.com>
2019-12-17 10:43:57 -0600

    head and add react-typed

--- src/views/Home.js
+++ src/views/Home.js
@@ -2,13 +2,14 @@ import React from 'react';
 import Layout from './../common/Layout';
 import Head from './../common/Head';
 import InfoSection from './../components/InfoSection';
+import TypeText from './../common/TypeText';
 
 const Home = () => {
     return ( 
         <>
             <Layout>
                 <Head title="Aprende idiomas usando el habla" />
-                <h1>Hola</h1>
+                <TypeText />
                 <InfoSection />
             </Layout>
         </>
